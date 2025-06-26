6a8877d77c9a6722b1dfd4238d3169fca872bf6e
Meghna Chhanwal <mjchhanwal39@gmail.com>
2025-06-26 21:29:23 +0530

    backend file update

--- backend/models/timing.js
+++ backend/models/timing.js
@@ -7,11 +7,11 @@ const breakSchema = new mongoose.Schema({
 
 const timingSchema = new mongoose.Schema({
   employee: { type: mongoose.Schema.Types.ObjectId, ref: "Employee" },
-  date: String,
-  checkin: String,
-  checkout: String,
-  status: String,
-  break: [breakSchema],
+  date: String,            // e.g., '2025-06-26'
+  checkIn: String,         // e.g., '09:30:00'
+  checkOut: String,        // e.g., '18:30:00'
+  status: String,          // "Active" / "Inactive"
+  breaks: [breakSchema],   // renamed from 'break' to 'breaks'
 });
 
 export default mongoose.model("Timing", timingSchema);
