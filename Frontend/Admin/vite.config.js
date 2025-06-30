34b131cd7eeb20ab5c387b1d5bf5ce07a2f4e01e
Meghna Chhanwal <mjchhanwal39@gmail.com>
2025-06-30 21:32:56 +0530

    Added project files

--- Frontend/Admin/vite.config.js
+++ Frontend/Admin/vite.config.js
@@ -1,35 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const isDev = mode === "development";
-
-  const cspMetaTag = isDev
-    ? `
-      <meta http-equiv="Content-Security-Policy" content="
-        default-src 'self';
-        script-src 'self' 'unsafe-inline' 'unsafe-eval';
-        style-src 'self' 'unsafe-inline';
-      ">
-    </head>`
-    : `
-      <meta http-equiv="Content-Security-Policy" content="
-        default-src 'self';
-        script-src 'self' 'unsafe-inline' 'unsafe-eval';
-        style-src 'self' 'unsafe-inline';
-        connect-src https://salescrmapp.onrender.com;
-      ">
-    </head>`;
+  const isDev = mode === 'development';
 
   return {
-    plugins: [
-      react(),
-      {
-        name: "inject-csp-meta",
-        transformIndexHtml(html) {
-          return html.replace("</head>", cspMetaTag);
-        },
-      },
-    ],
+    plugins: [react()],
+    server: {
+      headers: isDev
+        ? {
+            'Content-Security-Policy':
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src *",
+          }
+        : {},
+    },
   };
 });
