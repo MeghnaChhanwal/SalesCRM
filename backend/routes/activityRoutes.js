8c15616f5f6cea45b422231e0623207866ef4257
Meghna Chhanwal <mjchhanwal39@gmail.com>
2025-07-06 13:38:53 +0530

    update

--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -1,6 +1,11 @@
-import activityController from "../controllers/activityController.js";
+import {
+  getAdminRecentActivities,
+  getEmployeeActivity
+} from "../controllers/activityController.js";
 
 const router = express.Router();
 
-router.get("/admin", activityController.getAdminRecentActivities);
-router.get("/employee/:id", activityController.getEmployeeActivity);
+router.get("/admin", getAdminRecentActivities);
+router.get("/employee/:id", getEmployeeActivity);
+
+export default router;
