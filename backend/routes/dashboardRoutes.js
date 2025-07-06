876326f9507c27b7de60c0e34b4723c692b85507
Meghna Chhanwal <mjchhanwal39@gmail.com>
2025-07-06 12:44:21 +0530

    update

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -3,12 +3,12 @@ import {
   getDashboardOverview,
   getChartData,
 } from "../controllers/dashboardController.js";
-import { getAdminRecentActivities } from "../controllers/activityController.js";
+
 
 const router = express.Router();
 
 router.get("/overview", getDashboardOverview);
 router.get("/chart", getChartData);
-router.get("/activity/admin", getAdminRecentActivities);
+
 
 export default router;
