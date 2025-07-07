2a34279d74deb2b487307d681cbdd1ed7f79d9f8
Meghna Chhanwal <mjchhanwal39@gmail.com>
2025-07-07 13:50:11 +0530

    update

--- backend/utils/time.js
+++ backend/utils/time.js
@@ -1,16 +1,20 @@
 
+
+
 export const todayIST = () => {
   const now = new Date();
-  now.setTime(now.getTime() + 5.5 * 60 * 60 * 1000); // add 5.5 hours to UTC
-  return now.toISOString().split("T")[0];
+  now.setTime(now.getTime() + 5.5 * 60 * 60 * 1000); 
+  return now.toISOString().split("T")[0]; 
 };
+
 
 export const timeIST = () => {
   const now = new Date();
-  now.setTime(now.getTime() + 5.5 * 60 * 60 * 1000); // add 5.5 hours to UTC
-  return now.toLocaleTimeString("en-IN", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-  });
-};
+  now.setTime(now.getTime() + 5.5 * 60 * 60 * 1000); // add 5.5 hours for IST
+  return now
+    .toLocaleTimeString("en-IN", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    })
+    .toUpperCase(); 
